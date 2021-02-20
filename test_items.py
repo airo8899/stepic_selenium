923745e7b717d41eb07eec730bc644beb4697735
airo <diykovaa@gmail.com>
2021-02-21 00:51:19 +0300

    files added

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_button_add_to_basket(browser):
+    browser.get(link)
+    button = browser.find_elements_by_css_selector('.btn-add-to-basket')
+    assert len(button) != 0, 'Кнопки нет'
+    time.sleep(10)
